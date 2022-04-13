3e8d7553659d7a56dd7b2601296a26b50c1bc08e
Nikolaus Jäger-Grassl <n.jaegergrassl@outlook.com>
2022-04-13 11:43:52 +0200

    changed loggin in app.js

--- app.js
+++ app.js
@@ -16,18 +16,24 @@ console.log( 'Call app.js' );
 const fs = require('fs');
 const fileName = './public/config.json';
 const file = require(fileName);
-if (file && file.arguments && file.arguments.execute)
-    file.arguments.execute.url = process.env.APP_URL || "";
 
-if (file && file.configurationArguments && file.configurationArguments.publish)
-    file.configurationArguments.publish.url = process.env.APP_URL || "";
+console.info(`Replace placeholder url in config.json with=${process.env.APP_URL}`); 
+if (file && file.arguments && file.arguments.execute){
+  console.info(`Replace arguments.execute.url in config.json with=${process.env.APP_URL}`); 
+  file.arguments.execute.url = process.env.APP_URL;
+}
+    
+
+if (file && file.configurationArguments && file.configurationArguments.publish){
+  console.info(`Replace configurationArguments.publish.url in config.json with=${process.env.APP_URL}`); 
+  file.configurationArguments.publish.url = process.env.APP_URL;
+}    
 
 fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
     if (err)
         return console.log(err);
     console.log('updated config.json');
 });
-
 
 
 var app = express();
