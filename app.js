e2ec4f29aa249ab6815471d74d87cf55f00c0cc9
Nikolaus Jäger-Grassl <n.jaegergrassl@outlook.com>
2022-04-13 12:01:33 +0200

    corrected missing url path after replacement

--- app.js
+++ app.js
@@ -18,26 +18,26 @@ const fileName = './public/config.json';
 const file = require(fileName);
 
 if (file && file.arguments && file.arguments.execute){
-  console.info(`Set arguments.execute.url in config.json to=${process.env.APP_URL}`); 
-  file.arguments.execute.url = process.env.APP_URL;
+  console.info(`Set arguments.execute.url in config.json to=${process.env.APP_URL}/journeybuilder/execute`); 
+  file.arguments.execute.url = `${process.env.APP_URL}/journeybuilder/save`;
 }
     
 
 if (file && file.configurationArguments && file.configurationArguments.save){
-  console.info(`Set configurationArguments.save.url=${file.configurationArguments.save.url} in config.json to=${process.env.APP_URL}`); 
-  file.configurationArguments.save.url = process.env.APP_URL;
+  console.info(`Set configurationArguments.save.url=${file.configurationArguments.save.url} in config.json to=${process.env.APP_URL}/journeybuilder/save`); 
+  file.configurationArguments.save.url = `${process.env.APP_URL}/journeybuilder/save`;
 }    
 if (file && file.configurationArguments && file.configurationArguments.publish){
-  console.info(`Set configurationArguments.publish.url=${file.configurationArguments.publish.url} in config.json to=${process.env.APP_URL}`); 
-  file.configurationArguments.publish.url = process.env.APP_URL;
+  console.info(`Set configurationArguments.publish.url=${file.configurationArguments.publish.url} in config.json to=${process.env.APP_URL}/journeybuilder/publish`); 
+  file.configurationArguments.publish.url = `${process.env.APP_URL}/journeybuilder/save`;
 }
 if (file && file.configurationArguments && file.configurationArguments.stop){
-  console.info(`Set configurationArguments.stop.url=${file.configurationArguments.stop.url} in config.json to=${process.env.APP_URL}`); 
-  file.configurationArguments.stop.url = process.env.APP_URL;
+  console.info(`Set configurationArguments.stop.url=${file.configurationArguments.stop.url} in config.json to=${process.env.APP_URL}/journeybuilder/stop`); 
+  file.configurationArguments.stop.url = `${process.env.APP_URL}/journeybuilder/save`;
 }
 if (file && file.configurationArguments && file.configurationArguments.validate){
-  console.info(`Set configurationArguments.validate.url=${file.configurationArguments.validate.url} in config.json to=${process.env.APP_URL}`); 
-  file.configurationArguments.validate.url = process.env.APP_URL;
+  console.info(`Set configurationArguments.validate.url=${file.configurationArguments.validate.url} in config.json to=${process.env.APP_URL}/journeybuilder/validate`); 
+  file.configurationArguments.validate.url = `${process.env.APP_URL}/journeybuilder/save`;
 }
 
 fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
